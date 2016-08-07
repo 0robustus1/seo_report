884fed9093ffab1607e8bbd532cb3d51cc746bff
Tim Reddehase <robustus@rightsrestricted.com>
2016-08-07 20:20:39 +0200

    provide json-based microdata support
    This reads in microdata as good as it can. If it identifies dangling
    children, basically when there is an itemscope within another itemscope
    without providing an itemprop at the same time, it stores them
    appropriately in order to facilitate proper 'error'-reporting.

---
+++ lib/seo_report/extractions/microdata.rb
@@ -0,0 +1,112 @@
+module SeoReport::Extractions
+  module Microdata
+    def extract_microdata(document)
+      MicrodataExtractor.new(document).extract!
+    end
+
+    class MicrodataExtractor
+      attr_reader :document
+
+      def initialize(document)
+        @document = document
+      end
+
+      def extract!
+        document.at_xpath('/html/body').children.each do |child|
+          process(child)
+        end
+        {microdata: result_set}
+      end
+
+      protected
+      def process(child, level = 1)
+        if microdata_element?(child)
+          process_microdata(child, level)
+        else
+          child.children.each { |c| process(c, level) }
+        end
+      end
+
+      def process_microdata(child, level)
+        if microdata_scope?(child)
+          push_scope(child)
+          child.children.each { |c| process(c, level + 1) }
+          scope = wrap_scope!
+          store_as_dangling_if_necessary!(child, scope, level)
+          result_set << scope if level == 1
+        else
+          provide_data_for_itemprop(child)
+          child.children.each { |c| process(c, level) }
+        end
+      end
+
+      def store_as_dangling_if_necessary!(element, scope, level)
+        itemprop = element["itemprop"]
+        is_top_level = level == 1
+        is_assigned_with_itemprop = !itemprop.nil? && !itemprop.empty?
+        if !is_top_level && !is_assigned_with_itemprop
+          (current_scope[:@dangling_children] ||= []) << scope
+        end
+      end
+
+      def provide_data_for_itemprop(element)
+        data =
+          if element.name == "meta"
+            element["content"]
+          elsif %w(audio embed iframe img source track video).include?(element.name)
+            element["src"]
+          elsif %w(a area link).include?(element.name)
+            element["href"]
+          elsif element.name == "object"
+            element["data"]
+          elsif %w(data meter).include?(element.name)
+            element["value"]
+          else
+            element.text
+          end
+        current_scope[element["itemprop"]] = data
+      end
+
+      def push_scope(element)
+        new_scope = {
+          type: element["itemtype"]
+        }
+        if microdata_prop?(element)
+          current_scope[element["itemprop"]] = new_scope
+        end
+        scopes.push(new_scope)
+      end
+
+      def wrap_scope!
+        scopes.pop
+      end
+
+      def current_scope
+        scopes.last
+      end
+
+      def scopes
+        @scopes ||= []
+      end
+
+      def result_set
+        @result_set ||= []
+      end
+
+      def microdata_element?(element)
+        %w(itemscope itemtype itemprop).
+          any? { |attr| element.has_attribute?(attr) }
+      end
+
+      def microdata_scope?(element)
+        element.has_attribute?("itemscope")
+      end
+
+      def microdata_prop?(element)
+        element.has_attribute?("itemprop")
+      end
+    end
+
+    SeoReport::Report.register_extraction(:html, Microdata, :extract_microdata)
+  end
+end
